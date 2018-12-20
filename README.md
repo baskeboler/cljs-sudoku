02d3de439615dafd99b5c9cd7b835fb0050c6e33
Victor Gil <baskeboler@gmail.com>
2018-12-20 19:00:02 -0300

    improved pagination, added dockerfile, added more details to readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # cljs-sudoku
 a random sudoku solution generator in clojurescript
 
-try it out:
+## Running locally
+
+### Prerequisites
+
+- `node.js` version >= 8 must be installed
+- `java` version >= 8 must be installed
+
+Start a local instance with `npx shadow-cljs watch app`
+If port 8080 is not available the server will start on a different port as displayed on screen 
+Open your browser and point to http://localhost:8080 or whichever port the server started on.
+
+## Building a release
+
+`npx shadow-cljs release app` 
+This will build an optimized, production ready release under folder `public/`
--- src/main/cljs_sudoku/app.cljs
+++ src/main/cljs_sudoku/app.cljs
@@ -1,12 +1,14 @@
 (ns cljs-sudoku.app
   (:require ["react"]
             [reagent.core :as reagent :refer [atom render]]
+            [clojure.set :as sets :refer [difference union]]
             [cljs-sudoku.sudoku :as s :refer [random-sudoku neighbor-positions]]
             [cljs.core.async :as async :refer [<! >! chan put! go go-loop]
              :include-macros true]))
 
 (def current-view (atom :regular))
-            
+
+
 (defn generate-new [sudoku loading?]
   (fn [& opts]
     (reset! loading? true)
@@ -21,23 +23,60 @@
 
 (defn pagination
   [start end current fn-page]
-  [:nav.pagination
-   {:role :pagination
-    :aria-label :pagination}
-   [:a.pagination-previous {:on-click #(if (> @current @start) (fn-page (dec @current)))}
-    "previous"]
-   [:a.pagination-next {:on-click #(if (< @current @end) (fn-page (inc @current)))}
-    "next"]
-   [:ul.pagination-list
-    (doall
-     (for [i (range @start @end)]
-      [:li>a
-       {:key (str "item_" i)
-        :class ["pagination-link" (when (= @current i) "is-current")]
-        :on-click #(fn-page i)}
-       i]))]])
+  (let [paging-item (fn [page-num]
+                      [:li>a
+                       {
+                        :class ["pagination-link" (when (= @current page-num) "is-current")]
+                        :on-click #(fn-page page-num)}
+                       page-num])]
+   [:nav.pagination
+    {:role :pagination
+     :aria-label :pagination}
+    [:a.pagination-previous {:on-click #(if (> @current @start) (fn-page (dec @current)))}
+     "previous"]
+    [:a.pagination-next {:on-click #(if (< @current @end) (fn-page (inc @current)))}
+     "next"]
+    [:ul.pagination-list
+     (cond
+       (> 10 (- @end @start)) (doall
+                               (for [i (range @start @end)]
+                                 [paging-item i {:key (str "item_" i)}]))
+       (> 3 (- @current @start)) (doall
+                                  (concat
+                                   (for [i (range @start (+ 2 @current))
+                                         :when (> @end i)]
+                                     [paging-item i {:key (str "item_" i)}])
+                                   [[:li
+                                     [:span.pagination-ellipsis "..."]]
+                                    [paging-item (dec @end)]]))
+       (> 3 (- @end @current)) (doall
+                                (concat
+                                 [[paging-item @start]
+                                  [:li>span.ellipsis "..."]]
+                                 (for [i (range (- @current 1) @end)
+                                       :when (> @end i)]
+                                   [paging-item i {:key (str "item_" i)}])))
+       :else (doall
+              (concat
+               [[paging-item @start]
+                [:li>span.pagination-ellipsis "..."]]
+               (for [i (range (- @current 1) (+ @current 2))
+                     :when (> @end i)]
+                 [paging-item i {:key (str "item_" i)}])
+               [[:li>span.pagination-ellipsis "..."]
+                [paging-item (dec @end)]])))]]))
+
 (defn sudoku-component [sud]
-  (let [highlighted (atom #{})]
+  (let [highlighted (atom #{})
+        highlighted-secondary (atom #{})
+        cell-click-fn (fn [x y value]
+                        (fn []
+                          (swap! highlighted union (s/neighbor-positions x y))
+                          (swap! highlighted-secondary conj value)
+                          (go
+                            (<! (async/timeout 2000))
+                            (swap! highlighted difference (s/neighbor-positions x y))
+                            (swap! highlighted-secondary disj value))))] 
     (fn []
       (if @sud
         [:div.sudoku.card
@@ -49,12 +88,12 @@
              (for [[j n] (map-indexed #(vector %1 %2) r)]
               [:span.cell
                {:key (str "cell_" i "_" j)
-                :class ["cell" (when (@highlighted {:x j :y i}) "highlighted")]
-                :on-click #(do
-                             (reset! highlighted (s/neighbor-positions j i))
-                             (go
-                               (<! (async/timeout 2000))
-                               (reset! highlighted #{})))}
+                :class ["cell"
+                        (when (@highlighted {:x j :y i})
+                          "highlighted")
+                        (when (@highlighted-secondary n)
+                          "highlighted-secondary")]
+                :on-click (cell-click-fn j i n)}
                n]))]))]
         [:div "Apreta el boton"]))))
 
@@ -73,31 +112,10 @@
      [:h1
       "sudoku"]]
     [:div.navbar-item>a
-     {:on-click #(reset! current-view :regular)}
-     "regular"]
-    [:div.navbar-item>a
-     {:on-click #(reset! current-view :history)}
-     "history"]
-    #_[:div.navbar-item.is-pulled-right
-        [:button.button.is-primary
-          {:type :button
-           :on-click (generate-new sudoku loading?)
-           :class ["button"]
-               "is-secondary"
-               (when @loading?
-                 "is-loading")}
-         "Generar"]]]
-   #_[:div.navbar-menu.is-active
-      [:div.navbar-end
-       [:div.navbar-item
-        [:button.button.is-primary
-         {:type :button
-          :on-click (generate-new sudoku loading?)
-          :class ["button"
-                  "is-secondary"
-                  (when @loading?
-                    "is-loading")]}
-         "Generar"]]]]])
+     {:on-click #(if (= @current-view :regular)
+                   (reset! current-view :history)
+                   (reset! current-view :regular))}
+     (if (= @current-view :regular) "ver anteriores" "generá más")]]])
 
 (defn past-sudokus-view []
   (let [sudoku-vec (->> (vals @s/sudokus)
@@ -117,6 +135,7 @@
     (fn []
       [:div.section
        [pagination start end current page-fn]
+       [:hr]
        [sudoku-component current-sudoku]])))
 
 (defn regular-view [sudoku loading?]
